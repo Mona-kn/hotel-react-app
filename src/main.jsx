2c8745484611f1d5d1a36922db6c5a698ac9687c
Mona <142782181+Mona-kn@users.noreply.github.com>
2025-12-21 17:10:19 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,9 +13,11 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <HashRouter>
-      <App />
-   </HashRouter>
-  </ClerkProvider>,
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ClerkProvider>
+  </StrictMode>
 )
